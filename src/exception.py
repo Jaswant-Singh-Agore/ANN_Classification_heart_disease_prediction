8d2e17b88914472b2beeaeac5a5c68f13aac5646
Jaswant-Singh-Agore <jaswantagore@gmail.com>
2025-11-09 19:14:53 +0530

    adding source files

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+from src.logger import logging
+
+def error_message(error):
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    if exc_tb:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+    
+    return f"Error in [{file_name}] line [{line_number}]: {str(error)}"
+
+
+class CustomeException(Exception):
+    def __init__(self, error_message):
+        detailed = error_message(error_message)
+        logging.error(detailed)
+        super().__init__(detailed)
+        self.error_message = detailed
+        
+    
+    def __str__(self):
+        return self.error_message
---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+# Create logs directory if it doesn't exist
+LOGS_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+# log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
+
+#logging configuration
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(levelname)s - %(name)s - Line:%(lineno)d - %(message)s",
+    level=logging.INFO,
+)
+
